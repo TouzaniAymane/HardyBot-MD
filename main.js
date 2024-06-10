a3d3d9aac80f7fac816b2b1d2093ec7c75a3cd96
Aymane Touzani <158565484+gxaymenofc@users.noreply.github.com>
2024-06-10 12:27:39 +0100

    𒀂𒀀𒇲𒁓𒌨 - 𒁀𒆸𒈦
    Signed-off-by: Aymane Touzani <158565484+gxaymenofc@users.noreply.github.com>

--- main.js
+++ main.js
@@ -551,7 +551,7 @@ setInterval(async () => {
   if (stopped === 'close' || !conn || !conn.user) return;
   const _uptime = process.uptime() * 1000;
   const uptime = clockString(_uptime);
-  const bio = `[ ⏳ ] Uptime: ${uptime}`;
+  const bio = `[ ⏳ ] Uptime: ${uptime} | Created by Aymane Touzani`;
   await conn.updateProfileStatus(bio).catch((_) => _);
 }, 60000);
 function clockString(ms) {
